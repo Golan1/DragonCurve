e86d9c769cfacaf4ec1d89298536fdb8793cbc5c
Golan1 <golanlevi1@gmail.com>
2015-01-03 04:00:59 +0200

    Slow Mo!
    can switch between slowmo or all at once

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
 var renderer, scene, camera;
 
+var isSlowDrawing = false;
+
 var dragons = [];
 
-var lineLen = 0.1;
+var lineLen = 1;
 
 var directions = {
     0: [0, 1],
@@ -31,15 +33,39 @@ document.addEventListener("DOMContentLoaded", function () {
         dragons.push(d);
     }
 
-    render(dragons);
+    if (isSlowDrawing) {
+        scene = new THREE.Scene();
+        dragons.forEach(function (dragon) {
+            scene.add(new THREE.Line(dragon.getGeometry(), dragon.material));
+        });
+    }
+
+    animate();
 
 });
 
 function render(dragons) {
-    scene = new THREE.Scene();
-    dragons.forEach(function (dragon) {
-        scene.add(new THREE.Line(dragon.getGeometry(), dragon.material));
-    });
+
+    if (isSlowDrawing) {
+        var allDragonsCompleted = dragons.map(function (dragon) {
+            return dragon.isLevelCompleted();
+        }).reduce(function (completed, me) {
+            return completed && me;
+        });
+
+        if (!allDragonsCompleted) {
+            scene = new THREE.Scene();
+            dragons.forEach(function (dragon) {
+                scene.add(new THREE.Line(dragon.getGeometry(), dragon.material));
+            });
+        }
+    }
+    else {
+        scene = new THREE.Scene();
+        dragons.forEach(function (dragon) {
+            scene.add(new THREE.Line(dragon.getAllGeometry(), dragon.material));
+        });
+    }
 
     renderer.render(scene, camera);
 }
@@ -48,7 +74,7 @@ function nextCurves() {
     dragons.forEach(function (dragon) {
         dragon.nextLevel();
     });
-    render(dragons);
+    animate();
     //lineLen = lineLen / 1.3;
 }
 
@@ -59,16 +85,17 @@ function Dragon(firstDir, color) {
     this.vertices = [];
     this.vertices.push(new THREE.Vector3(0, 0, 0));
     this.vertices.push(new THREE.Vector3(directions[this.dir][0] * lineLen, directions[this.dir][1] * lineLen, 0));
+    this.index = 0;
 }
 
-Dragon.prototype.getGeometry = function () {
+Dragon.prototype.getAllGeometry = function () {
 
     var X = this.vertices[this.vertices.length - 1].x;
     var Y = this.vertices[this.vertices.length - 1].y;
 
-    for (var i = this.curves.length / 2 - 0.5; i < this.curves.length && this.curves.length; i++) {
+    for (; this.index < this.curves.length && this.curves.length; this.index++) {
 
-        this.dir = (this.dir + this.curves[i] + 4) % 4;
+        this.dir = (this.dir + this.curves[this.index] + 4) % 4;
 
         X += directions[this.dir][0] * lineLen;
         Y += directions[this.dir][1] * lineLen;
@@ -81,7 +108,33 @@ Dragon.prototype.getGeometry = function () {
     return geometry;
 }
 
-Dragon.prototype.nextLevel = function(){
+Dragon.prototype.getGeometry = function () {
+
+    var X = this.vertices[this.vertices.length - 1].x;
+    var Y = this.vertices[this.vertices.length - 1].y;
+
+    if (!this.isLevelCompleted()) {
+
+        this.dir = (this.dir + this.curves[this.index] + 4) % 4;
+
+        X += directions[this.dir][0] * lineLen;
+        Y += directions[this.dir][1] * lineLen;
+
+        this.vertices.push(new THREE.Vector3(X, Y, 0));
+
+        this.index++;
+    }
+
+    var geometry = new THREE.Geometry();
+    geometry.vertices = this.vertices;
+    return geometry;
+}
+
+Dragon.prototype.isLevelCompleted = function () {
+    return this.index == this.curves.length;
+}
+
+Dragon.prototype.nextLevel = function () {
     var newCurves = [];
     var i = 0;
     for (; i < this.curves.length; i++) {
@@ -92,8 +145,14 @@ Dragon.prototype.nextLevel = function(){
     this.curves = newCurves;
 }
 
-function animate(){
-    requestAnimationFrame( animate );
+function animate() {
+    if (isSlowDrawing) {
+        requestAnimationFrame(animate);
+    }
 
     render(dragons);
+}
+
+function slowmoChanged(){
+    isSlowDrawing =  document.getElementById('slowmo').checked;
 }
